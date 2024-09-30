a2b3751dfd91b098b72707d53ac9e82a671b2c24
hasanmoriya <developer.hasan429@gmail.com>
2024-09-30 18:14:30 +0530

    continue development

--- app/Livewire/Sliders.php
+++ app/Livewire/Sliders.php
@@ -3,11 +3,22 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\SiteBanner;
 
 class Sliders extends Component
 {
+    public $banners;
+
+    public function mount()
+    {
+        // Fetch the banners data from the SiteBanner model
+        $this->banners = SiteBanner::all(); // You can also use paginate() if needed
+    }
+
     public function render()
     {
-        return view('livewire.sliders');
+        return view('livewire.sliders', [
+            'banners' => $this->banners,
+        ]);
     }
 }

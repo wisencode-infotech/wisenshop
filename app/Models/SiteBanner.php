6abcb3d13fd64bbaafc2c5e8d79dd448ca321fa4
hasanmoriya <developer.hasan429@gmail.com>
2024-09-30 19:00:47 +0530

    continue development

--- app/Models/SiteBanner.php
+++ app/Models/SiteBanner.php
@@ -19,14 +19,15 @@ class SiteBanner extends Model
     // Define a placeholder URL
     public static $placeholder_url = 'https://placehold.co/445x205';
 
-    // Accessors
+
     public function getImageUrlAttribute()
     {
+        // Check if image_path is set and the file exists
         if ($this->image_path && Storage::disk('public')->exists($this->image_path)) {
             return Storage::url($this->image_path); // Return the URL of the stored image
         }
 
-        // Return the placeholder URL if the file does not exist
-        return $this->placeholder_url;
+        // If image_path is not set or file does not exist, return the placeholder URL
+        return self::$placeholder_url; // Ensure to use self:: for static property access
     }
 }

e9ff7e76c56aa0ebb80d93b8de062114eac58503
Mahammadali <manknojiya121@gmail.com>
2024-10-01 09:40:47 +0530

    UI touches

--- resources/views/livewire/contact-page.blade.php
+++ resources/views/livewire/contact-page.blade.php
@@ -1,5 +1,97 @@
 @section('title', 'Contact Us')
 
 <div>
-    <h1>Contact Page</h1>
+    <div
+        class="mx-auto flex w-full max-w-7xl flex-col px-5 py-10 pb-20 md:flex-row md:pb-10 xl:py-14 xl:px-8 xl:pb-14 2xl:px-14">
+        <div class="order-2 w-full shrink-0 rounded-lg bg-light p-5 md:order-1 md:w-72 lg:w-96">
+            <div class="mb-8 flex w-full items-center justify-center overflow-hidden"><img alt="Contact" loading="lazy"
+                    width="299" height="232" decoding="async" data-nimg="1" class="h-auto w-full"
+                    src="{{ asset('assets/frontend/img/static/media/contact-illustration.2f6adc05.svg') }}" style="color: transparent;"></div>
+            <div class="mb-8 flex flex-col"><span class="mb-3 font-semibold text-heading">Address</span><span
+                    class="text-sm text-body"><a class="" target="_blank" title="NY State Thruway, New York, USA"
+                        href="https://www.google.com/maps/place/NY State Thruway, New York, USA">NY State Thruway, New
+                        York, USA</a></span></div>
+            <div class="mb-8 flex flex-col"><span class="mb-3 font-semibold text-heading">Phone</span><span
+                    class="text-sm text-body">+129290122122</span></div>
+            <div class="mb-8 flex flex-col"><span class="mb-3 font-semibold text-heading">Email Address</span><span
+                    class="text-sm text-body">demo@demo.com</span></div>
+            <div class="mb-8 flex flex-col"><span class="mb-3 font-semibold text-heading">Website</span>
+                <div class="flex items-center justify-between"><span class="text-sm text-body">https://redq.io</span><a
+                        class="text-sm font-semibold text-accent hover:text-accent-hover focus:text-blue-500 focus:outline-none"
+                        target="_blank" href="https://redq.io">Visit This Site</a></div>
+            </div>
+            <div class="mb-8 flex flex-col"><span class="mb-4 font-semibold text-heading">Follow Us</span>
+                <div class="flex items-center justify-start"><a
+                        class="text-muted transition-colors duration-300 focus:outline-none ltr:mr-8 ltr:last:mr-0 rtl:ml-8 rtl:last:ml-0 hover:undefined"
+                        target="_blank" title="https://www.facebook.com/redqinc"
+                        href="https://www.facebook.com/redqinc"><svg xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 12 12" class="w-4 h-4">
+                            <path data-name="_ionicons_svg_logo-facebook (6)"
+                                d="M11.338 0H.662A.663.663 0 000 .663v10.674a.663.663 0 00.662.662H6V7.25H4.566V5.5H6V4.206a2.28 2.28 0 012.459-2.394c.662 0 1.375.05 1.541.072V3.5H8.9c-.753 0-.9.356-.9.881V5.5h1.794L9.56 7.25H8V12h3.338a.663.663 0 00.662-.663V.662A.663.663 0 0011.338 0z"
+                                fill="currentColor"></path>
+                        </svg></a><a
+                        class="text-muted transition-colors duration-300 focus:outline-none ltr:mr-8 ltr:last:mr-0 rtl:ml-8 rtl:last:ml-0 hover:undefined"
+                        target="_blank" title="https://twitter.com/RedqTeam" href="https://twitter.com/RedqTeam"><svg
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14.747 12" class="w-4 h-4">
+                            <path data-name="_ionicons_svg_logo-twitter (5)"
+                                d="M14.747 1.422a6.117 6.117 0 01-1.737.478A3.036 3.036 0 0014.341.225a6.012 6.012 0 01-1.922.734 3.025 3.025 0 00-5.234 2.069 2.962 2.962 0 00.078.691A8.574 8.574 0 011.026.553a3.032 3.032 0 00.941 4.044 2.955 2.955 0 01-1.375-.378v.037A3.028 3.028 0 003.02 7.225a3.046 3.046 0 01-.8.106 2.854 2.854 0 01-.569-.056 3.03 3.03 0 002.828 2.1 6.066 6.066 0 01-3.759 1.3 6.135 6.135 0 01-.722-.044A8.457 8.457 0 004.631 12a8.557 8.557 0 008.616-8.619c0-.131 0-.262-.009-.391a6.159 6.159 0 001.509-1.568z"
+                                fill="currentColor"></path>
+                        </svg></a><a
+                        class="text-muted transition-colors duration-300 focus:outline-none ltr:mr-8 ltr:last:mr-0 rtl:ml-8 rtl:last:ml-0 hover:undefined"
+                        target="_blank" title="https://www.instagram.com/redqteam"
+                        href="https://www.instagram.com/redqteam"><svg data-name="Group 96"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 12" class="w-4 h-4">
+                            <path data-name="Path 1"
+                                d="M8.5 1A2.507 2.507 0 0111 3.5v5A2.507 2.507 0 018.5 11h-5A2.507 2.507 0 011 8.5v-5A2.507 2.507 0 013.5 1h5m0-1h-5A3.51 3.51 0 000 3.5v5A3.51 3.51 0 003.5 12h5A3.51 3.51 0 0012 8.5v-5A3.51 3.51 0 008.5 0z"
+                                fill="currentColor"></path>
+                            <path data-name="Path 2"
+                                d="M9.25 3.5a.75.75 0 11.75-.75.748.748 0 01-.75.75zM6 4a2 2 0 11-2 2 2 2 0 012-2m0-1a3 3 0 103 3 3 3 0 00-3-3z"
+                                fill="currentColor"></path>
+                        </svg></a></div>
+            </div>
+        </div>
+        <div
+            class="order-1 mb-8 w-full rounded-lg bg-light p-5 md:order-2 md:mb-0 md:p-8 ltr:md:ml-7 rtl:md:mr-7 ltr:lg:ml-9 rtl:lg:mr-9">
+            <h1 class="mb-7 font-body text-xl font-bold text-heading md:text-2xl">How can we improve your experience?
+            </h1>
+            <form>
+                <div class="grid grid-cols-1 gap-5 sm:grid-cols-2">
+                    <div><label for="name"
+                            class="mb-3 block text-sm font-semibold leading-none text-body-dark">Name</label><input
+                            id="name" name="name" type="text"
+                            class="flex w-full appearance-none items-center px-4 text-sm text-heading transition duration-300 ease-in-out focus:outline-0 focus:ring-0 border border-border-base rounded focus:border-accent h-12"
+                            autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"
+                            aria-invalid="false" placeholder="Your name..."></div>
+                    <div><label for="email"
+                            class="mb-3 block text-sm font-semibold leading-none text-body-dark">Email</label><input
+                            id="email" name="email" type="email"
+                            class="flex w-full appearance-none items-center px-4 text-sm text-heading transition duration-300 ease-in-out focus:outline-0 focus:ring-0 border border-border-base rounded focus:border-accent h-12"
+                            autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"
+                            aria-invalid="false" placeholder="Your email..."></div>
+                </div>
+                <div class="my-5"><label for="subject"
+                        class="mb-3 block text-sm font-semibold leading-none text-body-dark">Subject</label><input
+                        id="subject" name="subject" type="text"
+                        class="flex w-full appearance-none items-center px-4 text-sm text-heading transition duration-300 ease-in-out focus:outline-0 focus:ring-0 border border-border-base rounded focus:border-accent h-12"
+                        autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"
+                        aria-invalid="false" placeholder="Your subject..."></div>
+                <div class="my-5"><label for="description"
+                        class="mb-3 block text-sm font-semibold leading-none text-body-dark">Description</label>
+                    <textarea id="description" name="description"
+                        class="flex w-full appearance-none items-center rounded px-4 py-3 text-sm text-heading transition duration-300 ease-in-out focus:outline-0 focus:ring-0 border border-border-base focus:border-accent"
+                        autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" rows="4"
+                        placeholder="Your message..." maxlength="150"></textarea>
+                </div>
+                <div class="text-[#1F2937] text-base font-medium">
+                    <div class="flex items-center"><input id="isChecked" name="isChecked" type="checkbox"
+                            class="checkbox"><label for="isChecked"
+                            class="text-[#1F2937] text-base font-medium text-body text-sm primary">I'd like to hear
+                            from Pickbazar in the future. (You can unsubscribe at any time)</label></div>
+                </div>
+                <div class="text-center mt-8"><button data-variant="normal"
+                        class="inline-flex items-center justify-center shrink-0 font-semibold leading-none rounded outline-none transition duration-300 ease-in-out focus:outline-0 focus:shadow focus:ring-1 focus:ring-accent-700 bg-accent text-light border border-transparent hover:bg-accent-hover px-6 py-5 text-sm font-bold uppercase">Send
+                        Message</button></div>
+            </form>
+        </div>
+    </div>
 </div>
--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="flex w-full flex-col border-t border-gray-800 border-t-border-100 bg-white px-5 md:px-10 lg:border-b-8 lg:px-[50px] xl:px-16">
    <div class="grid w-full grid-cols-[repeat(auto-fill,minmax(260px,1fr))] gap-6 pt-16 md:grid-cols-3 lg:pt-24 lg:pb-16 xl:grid-cols-[repeat(auto-fill,minmax(220px,1fr))] xl:gap-8 2xl:grid-cols-5">
       <div class="flex flex-col">
-         <div class="mb-[2px] flex h-16 items-start"><a class="inline-flex" href="/"><span class="relative h-[2.125rem] w-32 overflow-hidden md:w-[8.625rem]"><img alt="Pickbazar" loading="eager" decoding="async" data-nimg="fill" class="object-contain" sizes="(max-width: 768px) 100vw" srcset="{{ asset('assets/frontend/img/logo.webp') }}" src="{{ asset('assets/frontend/img/logo.webp') }}" style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;"></span></a></div>
+         <div class="mb-[2px] flex h-16 items-start"><a class="inline-flex" href="/"><span class="relative h-[2.125rem] w-32 overflow-hidden md:w-[8.625rem]"><img alt="Pickbazar" loading="eager" decoding="async" data-nimg="fill" class="object-contain" sizes="(max-width: 768px) 100vw" srcset="{{ asset('assets/frontend/img/logo.png') }}" src="{{ asset('assets/frontend/img/logo.webp') }}" style="position: absolute; height: 100%; width: 100%; inset: 0px; color: transparent;"></span></a></div>
          <a class="text-sm not-italic mb-7 text-heading" href="https://www.google.com/maps/place/NY State Thruway, New York, USA">NY State Thruway, New York, USA</a><a class="mb-1 text-sm text-heading" href="mailto:demo@demo.com">demo@demo.com</a><a class="text-sm text-heading" href="tel:+129290122122">+129290122122</a>
          <div>
             <div class="flex items-center gap-4 mt-4">
@@ -52,7 +52,7 @@
       </div>
       <div class="col-span-full md:col-span-2 lg:col-auto">
          <div class="flex flex-col">
-            <h3 class="mt-3 mb-7 text-xl font-semibold text-heading">Subscribe Now</h3>
+            {{-- <h3 class="mt-3 mb-7 text-xl font-semibold text-heading">Subscribe Now</h3>
             <p class="mb-7 text-sm text-heading">Subscribe your email for newsletter and featured news based on your interest</p>
             <div class="flex flex-col">
                <form novalidate="">
@@ -65,12 +65,12 @@
                      </button>
                   </div>
                </form>
-            </div>
+            </div> --}}
          </div>
       </div>
    </div>
    <div class="flex flex-col items-center w-full gap-2 pt-8 pb-20 mt-8 border-t border-gray-200 lg:mt-0 lg:flex-row lg:justify-between lg:border-t-0 lg:pb-12">
-      <span class="order-2 text-sm shrink-0 text-heading lg:order-1">©2024 <a class="font-medium text-heading" href="https://pickbazar.redq.io">Pickbazar</a>. Copyright © REDQ. All rights reserved worldwide. <a class="font-medium text-heading" href="https://redq.io">REDQ</a></span>
+      <span class="order-2 text-sm shrink-0 text-heading lg:order-1">©2024 <a class="font-medium text-heading" href="{{ env('APP_URL') }}">Pickbazar</a>. Copyright © REDQ. All rights reserved worldwide. <a class="font-medium text-heading" href="https://redq.io">REDQ</a></span>
       <div class="flex flex-wrap items-center justify-center order-1 gap-4 mb-5 lg:order-2 lg:mb-0 lg:justify-end lg:gap-x-5 lg:gap-y-3"></div>
    </div>
 </div>

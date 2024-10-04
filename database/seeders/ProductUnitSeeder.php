05f8e89aba9712555f1768293a75b66d234f2079
Akbar Husen <akbarmaknojiya@gmail.com>
2024-10-04 16:02:23 +0530

    soft delete

--- database/seeders/ProductUnitSeeder.php
+++ database/seeders/ProductUnitSeeder.php
@@ -10,12 +10,12 @@ class ProductUnitSeeder extends Seeder
     public function run()
     {
         $units = [
-            ['name' => 'Kilogram', 'short_name' => 'kg'],
-            ['name' => 'Gram', 'short_name' => 'g'],
-            ['name' => 'Liter', 'short_name' => 'L'],
-            ['name' => 'Milliliter', 'short_name' => 'ml'],
-            ['name' => 'Piece', 'short_name' => 'pc', 'can_have_variations' => '1'],
-            ['name' => 'Dozen', 'short_name' => 'dz'],
+            ['name' => 'Kilogram', 'short_name' => 'kg', 'can_have_variations' => '1'],
+            ['name' => 'Gram', 'short_name' => 'g', 'can_have_variations' => '1'],
+            ['name' => 'Liter', 'short_name' => 'L', 'can_have_variations' => '1'],
+            ['name' => 'Milliliter', 'short_name' => 'ml', 'can_have_variations' => '1'],
+            ['name' => 'Piece', 'short_name' => 'pc', 'can_have_variations' => '0'],
+            ['name' => 'Dozen', 'short_name' => 'dz', 'can_have_variations' => '1'],
         ];
 
         foreach ($units as $unit) {

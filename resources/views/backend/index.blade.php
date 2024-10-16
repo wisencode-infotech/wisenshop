b5731dff6efbee5388a4889b7d73e879d500550f
hasanmoriya <developer.hasan429@gmail.com>
2024-10-16 19:00:11 +0530

    continue development...

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -165,6 +165,26 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="col-md-3">
+                    <div class="card mini-stats-wid">
+                        <div class="card-body">
+                            <div class="media">
+                                <div class="media-body">
+                                    <p class="text-muted fw-medium">Total Credit</p>
+                                    <h4 class="mb-0">{{ __appCurrencySymbol() }}{{ auth()->user()->credit ?? 0 }}</h4>
+                                </div>
+
+                                <div class="mini-stat-icon avatar-sm rounded-circle bg-primary align-self-center">
+                                    <span class="avatar-title">
+                                        <i class="bx bx-dollar-circle font-size-24"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 @endif
                 <!-- <div class="col-md-3">
                     <div class="card mini-stats-wid">

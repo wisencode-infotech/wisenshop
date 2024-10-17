7d07e9cc3c7e99a0bb0b3032c70a4c120865b9ef
Mahammadali <manknojiya121@gmail.com>
2024-10-17 10:08:27 +0530

    Updates

--- resources/views/livewire/quantity-selector.blade.php
+++ resources/views/livewire/quantity-selector.blade.php
@@ -3,14 +3,14 @@
         <div>
             <template x-if="'{{ $layout }}' === 'cart'">
                 <div class="flex overflow-hidden flex-col-reverse items-center w-8 h-24 bg-accent text-heading rounded-full text-accent-contrast">
-                    <button @click="quantity--;$wire.decrement()" 
+                    <button @click="if(quantity > 0) { quantity--; $wire.decrement(); }" 
                             class="cursor-pointer p-2 transition-colors duration-200 hover:bg-accent-hover focus:outline-0 px-5">
                         <i class="fa fa-minus"></i>
                     </button>
                     <div class="flex flex-1 items-center justify-center px-3 text-sm font-semibold !px-0">
                         <span x-text="quantity"></span>
                     </div>
-                    <button @click="quantity++;$wire.increment()" 
+                    <button @click="quantity++; $wire.increment()" 
                             class="cursor-pointer p-2 transition-colors duration-200 hover:bg-accent-hover focus:outline-0 px-5">
                         <i class="fa fa-plus"></i>
                     </button>
@@ -20,14 +20,14 @@
             <template x-if="'{{ $layout }}' === 'large'">
                 <div class="mb-3 w-full lg:mb-0 lg:max-w-[400px]">
                     <div class="overflow-hidden w-full h-14 rounded text-light bg-accent inline-flex justify-between text-accent-contrast">
-                        <button @click="quantity--;$wire.decrement()" 
+                        <button @click="if(quantity > 0) { quantity--; $wire.decrement(); }" 
                                 class="cursor-pointer p-2 transition-colors duration-200 hover:bg-accent-hover focus:outline-0 px-5">
                             <i class="fa fa-minus"></i>
                         </button>
                         <div class="flex flex-1 items-center justify-center px-3 text-sm font-semibold">
                             <span x-text="quantity"></span>
                         </div>
-                        <button @click="quantity++;$wire.increment()" 
+                        <button @click="quantity++; $wire.increment()" 
                                 class="cursor-pointer p-2 transition-colors duration-200 hover:bg-accent-hover focus:outline-0 px-5">
                             <i class="fa fa-plus"></i>
                         </button>
@@ -37,14 +37,14 @@
 
             <template x-if="'{{ $layout }}' !== 'cart' && '{{ $layout }}' !== 'large'">
                 <div class="flex overflow-hidden order-5 sm:order-4 w-9 sm:w-24 h-24 sm:h-10 bg-accent text-light rounded-full flex-col-reverse sm:flex-row absolute sm:relative bottom-0 sm:bottom-auto ltr:right-0 rtl:left-0 ltr:sm:right-auto ltr:sm:left-auto text-accent-contrast">
-                    <button @click="quantity--;$wire.decrement()" 
+                    <button @click="if(quantity > 0) { quantity--; $wire.decrement(); }" 
                             class="cursor-pointer p-2 transition-colors duration-200 hover:bg-accent-hover focus:outline-0 text-sm">
                         <i class="fa fa-minus"></i>
                     </button>
                     <div class="flex flex-1 items-center justify-center px-3 text-sm font-semibold">
                         <span x-text="quantity"></span>
                     </div>
-                    <button @click="quantity++;$wire.increment()" 
+                    <button @click="quantity++; $wire.increment()" 
                             class="cursor-pointer p-2 transition-colors duration-200 hover:bg-accent-hover focus:outline-0 text-sm">
                         <i class="fa fa-plus"></i>
                     </button>

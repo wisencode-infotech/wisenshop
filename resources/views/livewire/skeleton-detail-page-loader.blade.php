033e4d2d555ecc42f86302ee6e79579e7b0bd8e1
hasanmoriya <developer.hasan429@gmail.com>
2024-10-18 15:28:59 +0530

    continue development...

--- resources/views/livewire/skeleton-detail-page-loader.blade.php
+++ resources/views/livewire/skeleton-detail-page-loader.blade.php
@@ -22,4 +22,29 @@
     <div class="animate-pulse">
         <div class="h-12 bg-gray-200 rounded w-1/2"></div>
     </div>
+
+     <!-- Image Placeholder -->
+    <div class="animate-pulse">
+        <div class="h-64 bg-gray-200 rounded-lg mb-6"></div>
+    </div>
+
+    <!-- Title Placeholder -->
+    <div class="animate-pulse mb-4">
+        <div class="h-8 bg-gray-200 rounded w-3/4"></div>
+    </div>
+
+    <!-- Description Placeholder -->
+    <div class="animate-pulse mb-6">
+        <div class="h-6 bg-gray-200 rounded w-full mb-3"></div>
+        <div class="h-6 bg-gray-200 rounded w-5/6 mb-2"></div>
+        <div class="h-6 bg-gray-200 rounded w-4/5"></div>
+        <div class="h-6 bg-gray-200 rounded w-3/4 mb-2"></div>
+        <div class="h-6 bg-gray-200 rounded w-4/5"></div>
+    </div>
+
+    <!-- Button Placeholder -->
+    <div class="animate-pulse">
+        <div class="h-12 bg-gray-200 rounded w-1/2"></div>
+    </div>
+
 </div>

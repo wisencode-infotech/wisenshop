8cba20d964ffc08247b69203986a15168ddfc12d
Mahammadali <manknojiya121@gmail.com>
2024-10-19 14:58:41 +0530

    Updates

--- public/assets/frontend/js/preferences.js
+++ public/assets/frontend/js/preferences.js
@@ -36,10 +36,34 @@ if (!_user_is_loggedin) {
     localStorage.setItem('wishlist', JSON.stringify({}));
 }
 
-Livewire.on('shoppingCartUpdated', function() {
-    syncLocalCartFromSessionCart();
-});
+document.addEventListener('livewire:init', () => {
+    Livewire.on('shoppingCartUpdated', (event) => {
+        syncLocalCartFromSessionCart();
+    });
 
-Livewire.on('wishListUpdated', function(data) {
-    localStorage.setItem('wishlist', JSON.stringify(data));
-});
+    Livewire.on('showNotification', detail => {
+        var detail = event.detail[0] ?? event.detail;
+        const type = detail.type || 'info'; // Default to 'info' type
+        const message = detail.message || 'Notification triggered';
+
+        if (typeof toastr !== 'undefined') {
+            toastr.options = {
+                positionClass: "toast-bottom-center",
+                timeOut: 10000,
+                closeButton: true,
+                progressBar: true,
+                onclick: function() {
+                    toastr.clear(); // Clear the toast on click
+                }
+            };
+            toastr[type](message); // Show the toast
+        } else {
+            alert(message); // Fallback to alert if toastr is not available
+        }
+    });
+
+    Livewire.on('wishListUpdated', function(data) {
+        localStorage.setItem('wishlist', JSON.stringify(data));
+    });
+
+ });
--- resources/views/livewire/notification-toast.blade.php
+++ resources/views/livewire/notification-toast.blade.php
@@ -1,25 +1,2 @@
 <div wire:ignore>
-    <script type="text/javascript">
-        // Use Livewire's lifecycle event to listen for the browser event
-        Livewire.on('showNotification', detail => {
-            var detail = event.detail[0] ?? event.detail;
-            const type = detail.type || 'info'; // Default to 'info' type
-            const message = detail.message || 'Notification triggered';
-
-            if (typeof toastr !== 'undefined') {
-                toastr.options = {
-                    positionClass: "toast-bottom-center",
-                    timeOut: 10000,
-                    closeButton: true,
-                    progressBar: true,
-                    onclick: function() {
-                        toastr.clear(); // Clear the toast on click
-                    }
-                };
-                toastr[type](message); // Show the toast
-            } else {
-                alert(message); // Fallback to alert if toastr is not available
-            }
-        });
-    </script>
 </div>

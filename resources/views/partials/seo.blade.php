8f0bb732ccf82b6344724ac20249d9a189d48639
Akbar Husen <akbarmaknojiya@gmail.com>
2024-10-19 17:55:35 +0530

    seo set

--- resources/views/partials/seo.blade.php
+++ resources/views/partials/seo.blade.php
@@ -2,6 +2,6 @@
     <meta name="description" content="@yield('meta:description', config('app.name'))">
 
     <meta property="og:title" content="@yield('title', config('app.name'))">
-    <meta property="og:description" content="@yield('meta:og:description', config('app.name'))">
+    <meta property="og:description" content="@yield('meta:description', config('app.name'))">
     <meta property="og:image" content="@yield('meta:og:image', asset(__setting('header_logo')))">
     <meta property="og:url" content="@yield('meta:og:url', url()->current())">

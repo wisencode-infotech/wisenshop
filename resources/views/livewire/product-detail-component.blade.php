ab95a183540c7a9e7bb3100140a55429c6643572
Mahammadali <manknojiya121@gmail.com>
2024-10-19 18:49:21 +0530

    Updates

--- resources/views/livewire/product-detail-component.blade.php
+++ resources/views/livewire/product-detail-component.blade.php
@@ -166,7 +166,7 @@
                 <div class="flex w-full flex-col divide-y divide-gray-200 divide-opacity-70 sm:flex-row sm:items-center sm:space-x-8 sm:divide-y-0 sm:divide-x rtl:sm:space-x-reverse rtl:sm:divide-x-reverse">
                     <div class="w-full pb-4 sm:w-auto sm:pb-0">
                         <span class="inline-flex shrink-0 items-center rounded-full bg-product-rating px-6 py-2 text-3xl font-semibold mb-4">
-                          {{ $product_rating }}
+                          {{ round($product->average_rating) }}
                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.056 24" class="h-6 w-6 ltr:ml-2 rtl:mr-2">
                               <g data-name="Group 36413" fill="currentColor">
                                  <path id="Path_22667" data-name="Path 22667" d="M19.474,34.679l-6.946-4.346L5.583,34.679a.734.734,0,0,1-1.1-.8L6.469,25.93.263,20.668a.735.735,0,0,1,.421-1.3l8.1-.566,3.064-7.6a.765.765,0,0,1,1.362,0l3.064,7.6,8.1.566a.735.735,0,0,1,.421,1.3L18.588,25.93l1.987,7.949a.734.734,0,0,1-1.1.8Z" transform="translate(0 -10.792)"></path>

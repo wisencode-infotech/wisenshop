c4ec522c9dbeccebd1b24a9c02f78711e388e2d9
hasanmoriya <developer.hasan429@gmail.com>
2024-10-22 10:47:49 +0530

    continue development...

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -116,7 +116,7 @@
 
                 <!-- Submit Button -->
                 <div class="text-center mt-6">
-                    <button type="submit"  class="inline-flex items-center justify-center flex-shrink-0 font-semibold leading-none rounded outline-none transition duration-300 ease-in-out focus:outline-none focus:shadow focus:ring-1 focus:ring-accent-700 bg-accent text-light border border-transparent hover:bg-accent-hover px-5 py-0 h-12 w-full uppercase">
+                    <button type="submit" wire:loading.attr="disabled" wire:target="profile_image" @if(!$temp_image_url) disabled @endif class="inline-flex items-center justify-center flex-shrink-0 font-semibold leading-none rounded outline-none transition duration-300 ease-in-out focus:outline-none focus:shadow focus:ring-1 focus:ring-accent-700 bg-accent text-light border border-transparent hover:bg-accent-hover px-5 py-0 h-12 w-full uppercase">
                         <span>{{ __trans('Update Profile') }}</span>
                     </button>
                 </div>

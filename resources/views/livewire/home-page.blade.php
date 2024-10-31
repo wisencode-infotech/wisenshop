2fe60b36a4d377b051ae9935cbbfe2732e9c3d7c
Mahammadali <manknojiya121@gmail.com>
2024-10-31 19:06:03 +0530

    Improvements

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -17,29 +17,6 @@
 
 @section('scripts')
 
-<!-- mobile category filter js  -->
-<script>
-     function initializeCategoryDropdown() {
-        // Toggle dropdown visibility on button click
-        $('.mobile-category-dropdown-btn').on('click', function () {
-            console.log(1);
-            $('.mobile-category-dropdown').toggleClass('hidden');
-        });
+<script src="{{ asset('assets/frontend/js/mobile-category-filter.js') }}" defer></script>
 
-        // Add click event listener to each category item
-        $('[role="custom-menuitem"] span').on('click', function () {
-            const categoryImage = $(this).find('img').attr('src');
-            const categoryName = $(this).find('.category-name').text();
-
-            $('.category-image-selected').attr('src', categoryImage);
-            $('.selected-category-name').text(categoryName);
-            $('.mobile-category-dropdown').addClass('hidden');
-        });
-    }
-
-    // Initialize dropdown when DOM is ready
-    $(document).ready(function () {
-        initializeCategoryDropdown();
-    });
-</script>
 @endsection

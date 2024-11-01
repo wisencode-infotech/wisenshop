ab8c3d35737f6ef81f0cb40f00dadc24764d192b
Mahammadali <manknojiya121@gmail.com>
2024-11-01 10:11:09 +0530

    Improvements

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -9,7 +9,7 @@
     </div>
 
     <div class="flex border-t border-solid border-border-200 border-opacity-70">
-        <livewire:product-category-sidebar :default_categories="$default_categories" lazy />
+        <livewire:product-category-sidebar :default_categories="$default_categories" />
         <livewire:products lazy :category_id="$default_categories" />
     </div>
     <livewire:product-cart-button lazy />

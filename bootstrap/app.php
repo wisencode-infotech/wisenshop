6f660d0fff84e8471aa9c5210a3d6314f6c8927e
Mahammadali <manknojiya121@gmail.com>
2024-11-15 18:04:04 +0530

    Improvements

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function () {
-            Route::middleware(['web', 'locale', 'is_install'])
+            // Route::middleware(['web', 'locale', 'is_install'])
+            Route::middleware(['web', 'locale'])
                 ->prefix('/')
                 ->name('frontend.')
                 ->group(base_path('routes/frontend.php'));

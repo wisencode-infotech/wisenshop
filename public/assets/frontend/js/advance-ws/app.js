9370b5770ecd9821d2309187150c8561ababc1bc
Mahammadali <manknojiya121@gmail.com>
2024-11-20 18:28:25 +0530

    Improvements

---
+++ public/assets/frontend/js/advance-ws/app.js
@@ -0,0 +1,117 @@
+$(document).on('click', '.mobile-filter-btn', function () {
+  const filter_section = $('.mobile-header-filter-section');
+  const mobile_filter_products_drawer_section = $('.mobile-filter-products-drawer-section');
+  filter_section.removeClass('hidden');
+  mobile_filter_products_drawer_section.removeClass('hidden');
+});
+
+$(document).on('click', '.mobile-header-filter-close-btn', function () {
+  const filter_section = $('.mobile-header-filter-section');
+  const mobile_filter_products_drawer_section = $('.mobile-filter-products-drawer-section');
+  filter_section.addClass('hidden');
+  mobile_filter_products_drawer_section.addClass('hidden');
+});
+
+$(document).on('click', '.mobile-pages-drawer-btn', function () {
+  const filter_section = $('.mobile-header-filter-section');
+  const mobile_filter_products_drawer_section = $('.mobile-pages-drawer-section');
+  filter_section.removeClass('hidden');
+  mobile_filter_products_drawer_section.removeClass('hidden');
+});
+
+$(document).on('click', '.mobile-header-filter-close-btn', function () {
+  const filter_section = $('.mobile-header-filter-section');
+  const mobile_filter_products_drawer_section = $('.mobile-pages-drawer-section');
+  filter_section.addClass('hidden');
+  mobile_filter_products_drawer_section.addClass('hidden');
+});
+
+$(document).on('click', '.top-product-search-btn', function () {
+  const filter_section = document.querySelector('.top-product-search-bar');
+  filter_section.classList.remove('hidden');
+});
+
+$(document).on('click', '.remove-search-filter', function () {
+  const filter_section = document.querySelector('.top-product-search-bar');
+  filter_section.classList.add('hidden');
+});
+
+$(document).on('click', '.products-filter', function () {
+  $('.mobile-pages-drawer-section').hide();
+  const filter_section = $('.mobile-header-filter-section');
+  const mobile_filter_products_drawer_section = $('.mobile-filter-products-drawer-section');
+  filter_section.removeClass('hidden');
+  mobile_filter_products_drawer_section.removeClass('hidden');
+});
+
+$(document).on('click', '.mobile-bottom-menu', function () {
+  setAllMobileMenusAsInactiveButThis(this);
+});
+
+function setAllMobileMenusAsInactiveButThis(menu_element = null) {
+  $(".mobile-bottom-menu").removeClass('text-accent');
+  if (menu_element != null && typeof menu_element != 'undefined')
+    $(menu_element).addClass('text-accent');
+}
+
+$(document).on('click', function (event) {
+  if (!$(event.target).closest('.profile_menu_section, .profile_menu_btn').length) {
+    $('.profile_menu_section').addClass('hidden');
+  }
+  if (!$(event.target).closest('.notification_menu_section, .notification_menu_btn').length) {
+    $('.notification_menu_section').addClass('hidden');
+  }
+});
+
+$('.profile_menu_btn').on('click', function () {
+  $('.profile_menu_section').toggleClass('hidden');
+});
+
+$('.notification_menu_btn').on('click', function () {
+  $('.notification_menu_section').toggleClass('hidden');
+});
+
+$('#grocery-search-header').on('focus', function() {
+  var scrollValue;
+
+  if (window.innerWidth <= 380) {  
+    scrollValue = 200;  
+  }else if (window.innerWidth <= 576) { 
+    scrollValue = 220;  
+  } else if (window.innerWidth <= 768) { 
+    scrollValue = 220; 
+  } else if (window.innerWidth <= 992) { 
+    scrollValue = 220; 
+  }else if (window.innerWidth <= 1039) { 
+    scrollValue = 190;  
+  } else if (window.innerWidth <= 1200) { 
+    scrollValue = 220; 
+  } else { 
+    scrollValue = 220;
+  }
+
+  console.log(window.innerWidth);
+  $('html, body').animate({ scrollTop: scrollValue }, 500);
+});
+
+$(document).on('click', '.clear-search', function () {
+  $('#grocery-search-header').val('');
+});
+
+$(document).on('click', '.wishlist-button', function (e) {
+  e.preventDefault();
+  let inWishlist = $(this).data('in-wishlist') === 'true';
+
+  if (inWishlist) {
+      $(this).find('i').removeClass('fa-solid fa-heart text-accent-500')
+                       .addClass('fa-regular fa-heart');
+      $(this).removeClass('border-active').addClass('border-gray-300');
+  } else {
+      $(this).find('i').removeClass('fa-regular fa-heart')
+                       .addClass('fa-solid fa-heart text-accent-500');
+      $(this).removeClass('border-gray-300').addClass('border-active');
+  }
+  
+  $(this).data('in-wishlist', !inWishlist);
+});
+
---
+++ public/assets/frontend/js/advance-ws/mobile-category-filter.js
@@ -0,0 +1,21 @@
+function initializeCategoryDropdown() {
+    // Toggle dropdown visibility on button click
+    $('.mobile-category-dropdown-btn').on('click', function () {
+        $('.mobile-category-dropdown').toggleClass('hidden');
+    });
+
+    // Add click event listener to each category item
+    $('[role="custom-menuitem"] span').on('click', function () {
+        const categoryImage = $(this).find('img').attr('src');
+        const categoryName = $(this).find('.category-name').text();
+
+        $('.category-image-selected').attr('src', categoryImage);
+        $('.selected-category-name').text(categoryName);
+        $('.mobile-category-dropdown').addClass('hidden');
+    });
+}
+
+// Initialize dropdown when DOM is ready
+$(document).ready(function () {
+    initializeCategoryDropdown();
+});

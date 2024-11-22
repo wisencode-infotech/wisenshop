854de0beb3880b9000fba19b404834535d059bc2
Akbarhusen <akbarmaknojiya@gmail.com>
2024-11-22 10:13:59 +0530

    advance theme development

--- resources/views/livewire/advance-ws/product-detail-component.blade.php
+++ resources/views/livewire/advance-ws/product-detail-component.blade.php
@@ -1,38 +1,59 @@
 <div>
-    <div class="max-w-4xl mx-auto">
+    <div class="max-w-4xl mx-auto mb-10">
         <div class="flex justify-between place-items-center py-4 px-1 mb-4">
             <div
-                class="w-9 h-9 shadow-lg bg-white text-cusblack hover:bg-cusblack hover:text-white duration-200 cursor-pointer rounded-full flex justify-center place-items-center">
+                class="w-9 h-9 shadow-lg bg-white text-cusblack hover:bg-cusblack hover:text-white duration-200 cursor-pointer rounded-full flex justify-center place-items-center" wire:navigate href="{{ route('frontend.home') }}" title="Back">
                 <svg class="w-4 h-4 " fill="none" stroke="currentColor" viewBox="0 0 24 24"
                     xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
-                </svg></div>
-            <h4 class="text-cusblack text-md">Product Details</h4>
+                </svg>
+            </div>
+            <h4 class="text-cusblack text-md">{{ __trans('Product Details') }}</h4>
             <div class="w-8"></div>
         </div>
         <div class="w-full bg-white md:rounded-2xl shadow-lg md:py-8 md:px-10 md:flex overflow-hidden">
             <div class="photo md:w-1/3">
-                <div><img class=" h-60 object-cover w-full md:rounded-2xl"
-                        src="https://i.ibb.co/0VnWhNb/dri-fit-classic-basketball-jersey-g-Z1-Bk0.jpg" alt="">
+                <div>
+                    @if(!$product->images->isEmpty())
+                        <img class="h-60 object-cover w-full md:rounded-2xl"
+                        src="{{ $product->images[0]->image_url }}" alt="">
+                    @else
+                        <div class="h-60 w-full md:rounded-2xl bg-gray-200"></div>
+                    @endif
                 </div>
-                <div class="px-2 md:px-0 flex mt-4"><img
-                        src="https://i.ibb.co/0VnWhNb/dri-fit-classic-basketball-jersey-g-Z1-Bk0.jpg"
+                <div class="px-2 md:px-0 flex mt-4">
+                    @if($product->images->isEmpty())
+                    @else
+                        @foreach($product->images as $image)
+                            <img
+                        src="{{ $image->image_url }}"
                         class="border-2 border-cusblack filter brightness-90  md:w-14 md:h-14 h-16 w-16 rounded-xl object-cover mr-3 cursor-pointer duration-100 "
-                        alt=""><img src="https://i.ibb.co/ccbzTbg/nike-m-dry-classic-jersey.jpg"
-                        class=" md:w-14 md:h-14 h-16 w-16 rounded-xl object-cover mr-3 cursor-pointer duration-100 "
-                        alt=""></div>
+                        alt="">
+                        @endforeach
+                    @endif
+                </div>
             </div>
             <div class="detail px-2 md:px-0 mt-3 md:mt-0 md:ml-6 py-2 md:w-2/3">
-                <p class="flex place-items-center text-sm text-gray-400">Jerseys and Kits<span class="mx-1"><svg
+                <p class="flex place-items-center text-sm text-gray-400">{{ $product->category->name }}
+                    {{-- <span class="mx-1"><svg
                             class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd"
                                 d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                                 clip-rule="evenodd"></path>
-                        </svg></span>Nike product</p>
-                <h1 class="text-3xl text-cusblack font-medium my-3">Nike Dri-FIT Classic Basketball Jersey</h1>
+                        </svg></span>
+                        Nike product --}}
+                    </p>
+                <h1 class="text-3xl text-cusblack font-medium my-3">{{ $product->name }}</h1>
                 <p class="text-sm text-gray-400">Black/Red</p>
-                <p class="my-3 font-semibold text-lg text-cusblack">Rp759,000</p>
-                <div class="sizes text-sm text-gray-400">
+                <p class="my-3 font-semibold text-lg text-cusblack">
+                    @livewire('product-price', ['product_id' => $product->id])
+                </p>
+                @if ($product->variations()->count() > 0)
+                    <div class="sizes text-sm text-gray-400">
+                        @livewire('product-variation', ['product_id' => $product->id])
+                    </div>
+                @endif
+                {{-- <div class="sizes text-sm text-gray-400">
                     <p class="mb-2">Select size</p>
                     <div class="flex"><button
                             class="bg-cusblack text-white mr-2 duration-200 flex place-items-center justify-center rounded-full w-12 h-12 cursor-pointer hover:bg-cusblack hover:text-white">S</button><button
@@ -40,7 +61,7 @@
                             class="text-cusblack border border-cusblack mr-2 duration-200 flex place-items-center justify-center rounded-full w-12 h-12 cursor-pointer hover:bg-cusblack hover:text-white">L</button><button
                             class="text-cusblack border border-cusblack mr-2 duration-200 flex place-items-center justify-center rounded-full w-12 h-12 cursor-pointer hover:bg-cusblack hover:text-white">XL</button>
                     </div>
-                </div>
+                </div> --}}
                 <div class="buttoncart flex mt-5 w-full"><button
                         class="w-4/5 md:w-3/5 bg-cusblack overflow-hidden py-4 text-white rounded-lg text-sm active:bg-gray-800 duration-100"><span
                             class="flex justify-center place-items-center overflow-hidden" style="transform: none;">Add
@@ -49,7 +70,8 @@
                                     <path
                                         d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z">
                                     </path>
-                                </svg></span></span></button><button
+                                </svg></span></span></button>
+                                <button
                         class="w-1/5 ml-2 bg-white border border-cusblack py-4 text-cusblack rounded-lg text-sm"><svg
                             class="w-5 h-5 m-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg">

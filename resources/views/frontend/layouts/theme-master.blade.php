e3d4c6cc7668f73ecc1217c4addc6212ab125091
Mahammadali <manknojiya121@gmail.com>
2024-11-22 09:46:28 +0530

    Improvements

--- resources/views/frontend/layouts/theme-master.blade.php
+++ resources/views/frontend/layouts/theme-master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ __appActiveTheme() }} @yield('title', config('app.name'))</title>
+    <title>@yield('title', config('app.name'))</title>
 
     @include('partials.seo')
 

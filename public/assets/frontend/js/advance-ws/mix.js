44b35a12224ec4dcb12d35f6aa2618ad6ccdb90c
Mahammadali <manknojiya121@gmail.com>
2024-11-23 14:29:53 +0530

    Improvements

--- public/assets/frontend/js/advance-ws/mix.js
+++ public/assets/frontend/js/advance-ws/mix.js
@@ -98,14 +98,6 @@ document.addEventListener('livewire:init', () => {
         }
     });
 
-    $(document).on('click', '.wc-tabs li, .tab-link-title', function(){
-      var tab_id = $(this).attr('data-tab');
-      $('.wc-tabs li, .tab-link-title').removeClass('active');
-      $('.tabs-entry-content').removeClass('active');
-      $(this).addClass('active');
-      $("#"+tab_id).addClass('active');
-    });
-
  });
 /*
  * Note that this is toastr v2.1.3, the "latest" version in url has no more maintenance,

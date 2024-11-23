d0399a7e0370969e7fadab9d08fa6586f8cd3095
Mahammadali <manknojiya121@gmail.com>
2024-11-23 11:50:27 +0530

    Improvements

--- resources/views/frontend/layouts/themes/advance-ws/partials/head/before-mix.blade.php
+++ resources/views/frontend/layouts/themes/advance-ws/partials/head/before-mix.blade.php
@@ -1,1 +1,4 @@
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,200&display=swap" rel="stylesheet">
+
+<!-- Themify Icons -->
+<link href="https://cdn.jsdelivr.net/npm/@icon/themify-icons@1.0.1-alpha.3/themify-icons.min.css" rel="stylesheet">

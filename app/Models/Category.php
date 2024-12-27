0cf13a665806a1d41bbfc2f7e0d18b842306444a
Mahammadali <manknojiya121@gmail.com>
2024-12-27 12:57:48 +0530

    Improvements

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -33,27 +33,33 @@ class Category extends Model
     // Accessors
     public function getImageUrlAttribute()
     {
-        // Check if image_path is set and the file exists
-        if ($this->image_path && Storage::disk('public')->exists($this->image_path)) {
-            return Storage::disk('public')->url($this->image_path); // Return the URL of the stored image
-        }
+        return cache()->rememberForever("category_{$this->id}_image_url", function () {
+            if ($this->image_path && Storage::disk('public')->exists($this->image_path)) {
+                return Storage::disk('public')->url($this->image_path);
+            }
 
-        // If image_path is not set or file does not exist, return the placeholder URL
-        return self::$placeholder_url; // Ensure to use self:: for static property access
+            return self::$placeholder_url;
+        });
+    }
+
+    public function getHasChildrenAttribute()
+    {
+        return cache()->rememberForever("category_{$this->id}_has_children", function () {
+            return $this->subcategories()->exists();
+        });
     }
 
     public function getTotalProductsAttribute()
     {
-        if ($this->parent_id > 0)
-            return $this->products()->count();
-    
-        $total_items = 0;
-        
-        foreach ($this->subcategories as $sub_category) {
-            $total_items += $sub_category->products()->count();
-        }
+        return cache()->rememberForever("category_{$this->id}_total_products", function () {
+            if (!$this->has_children) {
+                return $this->products()->count();
+            }
 
-        return $total_items;
+            return $this->subcategories->sum(function ($subcategory) {
+                return $subcategory->products()->count();
+            });
+        });
     }
 
     // scope
